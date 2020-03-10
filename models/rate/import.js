0a76ad0fb6117f8bbc11a1fdce67cf3dee1687e7
Khang Duy <trinhduykhang1313@gmail.com>
2020-03-10 10:38:30 +0700

    update rate on 2020-03-06

--- models/rate/import.js
+++ models/rate/import.js
@@ -1,61 +1,423 @@
 exports.rates = [
-  { bank: "BacABank", value: 7.55, threshold: "", period: 6 },
-  { bank: "BacABank", value: 7.6, threshold: "", period: 9 },
-  { bank: "BacABank", value: 7.8, threshold: "", period: 12 },
-  { bank: "BacABank", value: 7.9, threshold: "", period: 13 },
-  { bank: "BacABank", value: 7.9, threshold: "", period: 18 },
-  { bank: "BacABank", value: 7.9, threshold: "", period: 24 },
-  { bank: "BacABank", value: 7.9, threshold: "", period: 36 },
+  {
+    bank: "BacABank",
+    value: 7.55,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 6
+  },
+  {
+    bank: "BacABank",
+    value: 7.6,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 9
+  },
+  {
+    bank: "BacABank",
+    value: 7.8,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 12
+  },
+  {
+    bank: "BacABank",
+    value: 7.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 13
+  },
+  {
+    bank: "BacABank",
+    value: 7.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 18
+  },
+  {
+    bank: "BacABank",
+    value: 7.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 24
+  },
+  {
+    bank: "BacABank",
+    value: 7.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 36
+  },
 
-  { bank: "LienVietPostBank", value: 6.1, threshold: "", period: 9 },
-  { bank: "LienVietPostBank", value: 6.8, threshold: "", period: 12 },
-  { bank: "LienVietPostBank", value: 6.8, threshold: "", period: 13 },
-  { bank: "LienVietPostBank", value: 6.9, threshold: "", period: 18 },
-  { bank: "LienVietPostBank", value: 6.9, threshold: "", period: 24 },
-  { bank: "LienVietPostBank", value: 6.9, threshold: "", period: 36 },
+  {
+    bank: "LienVietPostBank",
+    value: 6.1,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 9
+  },
+  {
+    bank: "LienVietPostBank",
+    value: 6.8,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 12
+  },
+  {
+    bank: "LienVietPostBank",
+    value: 6.8,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 13
+  },
+  {
+    bank: "LienVietPostBank",
+    value: 6.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 18
+  },
+  {
+    bank: "LienVietPostBank",
+    value: 6.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 24
+  },
+  {
+    bank: "LienVietPostBank",
+    value: 6.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 36
+  },
 
-  { bank: "MSB", value: 6.8, threshold: "", period: 6 },
-  { bank: "MSB", value: 6.9, threshold: "", period: 9 },
+  {
+    bank: "MSB",
+    value: 6.8,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 6
+  },
+  {
+    bank: "MSB",
+    value: 6.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 9
+  },
 
-  { bank: "NCB", value: 4.9, threshold: "", period: 1 },
-  { bank: "NCB", value: 4.9, threshold: "", period: 2 },
-  { bank: "NCB", value: 7.6, threshold: "", period: 6 },
-  { bank: "NCB", value: 7.65, threshold: "", period: 9 },
-  { bank: "NCB", value: 8.1, threshold: "", period: 12 },
-  { bank: "NCB", value: 8.25, threshold: "", period: 13 },
-  { bank: "NCB", value: 8.25, threshold: "", period: 18 },
-  { bank: "NCB", value: 8.4, threshold: "", period: 24 },
-  { bank: "NCB", value: 8.4, threshold: "", period: 36 },
+  {
+    bank: "NCB",
+    value: 4.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 1
+  },
+  {
+    bank: "NCB",
+    value: 4.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 2
+  },
+  {
+    bank: "NCB",
+    value: 7.6,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 6
+  },
+  {
+    bank: "NCB",
+    value: 7.65,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 9
+  },
+  {
+    bank: "NCB",
+    value: 8.1,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 12
+  },
+  {
+    bank: "NCB",
+    value: 8.25,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 13
+  },
+  {
+    bank: "NCB",
+    value: 8.25,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 18
+  },
+  {
+    bank: "NCB",
+    value: 8.4,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 24
+  },
+  {
+    bank: "NCB",
+    value: 8.4,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 36
+  },
 
-  { bank: "OCB", value: 4.8, threshold: "", period: 1 },
-  { bank: "OCB", value: 4.9, threshold: "", period: 2 },
-  { bank: "OCB", value: 7.2, threshold: "", period: 6 },
-  { bank: "OCB", value: 7.3, threshold: "", period: 9 },
-  { bank: "OCB", value: 7.4, threshold: "", period: 12 },
-  { bank: "OCB", value: 7.5, threshold: "", period: 18 },
-  { bank: "OCB", value: 7.7, threshold: "", period: 24 },
-  { bank: "OCB", value: 7.7, threshold: "", period: 36 },
+  {
+    bank: "OCB",
+    value: 4.8,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 1
+  },
+  {
+    bank: "OCB",
+    value: 4.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 2
+  },
+  {
+    bank: "OCB",
+    value: 7.2,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 6
+  },
+  {
+    bank: "OCB",
+    value: 7.3,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 9
+  },
+  {
+    bank: "OCB",
+    value: 7.4,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 12
+  },
+  {
+    bank: "OCB",
+    value: 7.5,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 18
+  },
+  {
+    bank: "OCB",
+    value: 7.7,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 24
+  },
+  {
+    bank: "OCB",
+    value: 7.7,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 36
+  },
 
-  { bank: "Techcombank", value: 4.75, threshold: "", period: 1 },
-  { bank: "Techcombank", value: 4.75, threshold: "", period: 2 },
-  { bank: "Techcombank", value: 4.75, threshold: "", period: 3 },
-  { bank: "Techcombank", value: 6.8, threshold: "", period: 6 },
-  { bank: "Techcombank", value: 6.1, threshold: "", period: 9 },
-  { bank: "Techcombank", value: 6.9, threshold: "", period: 12 },
-  { bank: "Techcombank", value: 6.4, threshold: "", period: 13 },
-  { bank: "Techcombank", value: 7.1, threshold: "", period: 18 },
-  { bank: "Techcombank", value: 6.5, threshold: "", period: 24 },
+  {
+    bank: "Techcombank",
+    value: 4.75,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 1
+  },
+  {
+    bank: "Techcombank",
+    value: 4.75,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 2
+  },
+  {
+    bank: "Techcombank",
+    value: 4.75,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 3
+  },
+  {
+    bank: "Techcombank",
+    value: 6.8,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 6
+  },
+  {
+    bank: "Techcombank",
+    value: 6.1,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 9
+  },
+  {
+    bank: "Techcombank",
+    value: 6.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 12
+  },
+  {
+    bank: "Techcombank",
+    value: 6.4,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 13
+  },
+  {
+    bank: "Techcombank",
+    value: 7.1,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 18
+  },
+  {
+    bank: "Techcombank",
+    value: 6.5,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 24
+  },
   {
     bank: "Techcombank",
     value: 6.5,
     threshold: "Từ 3 tỉ và KH trên 50 tuổi",
+    lastUpdate: "2020-03-04",
     period: 36
   },
 
-  { bank: "TPBank", value: 4.75, threshold: "", period: 1 },
-  { bank: "TPBank", value: 4.85, threshold: "", period: 2 },
-  { bank: "TPBank", value: 4.95, threshold: "", period: 3 },
-  { bank: "TPBank", value: 6.4, threshold: "", period: 6 },
-  { bank: "TPBank", value: 6.9, threshold: "", period: 9 },
-  { bank: "TPBank", value: 7.6, threshold: "", period: 36 }
+  {
+    bank: "TPBank",
+    value: 4.75,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 1
+  },
+  {
+    bank: "TPBank",
+    value: 4.85,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 2
+  },
+  {
+    bank: "TPBank",
+    value: 4.95,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 3
+  },
+  {
+    bank: "TPBank",
+    value: 6.4,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 6
+  },
+  {
+    bank: "TPBank",
+    value: 6.9,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 9
+  },
+  {
+    bank: "TPBank",
+    value: 7.6,
+    threshold: "",
+    lastUpdate: "2020-03-04",
+    period: 36
+  },
+
+  {
+    bank: "ABBANK",
+    value: 4.8,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 1
+  },
+  {
+    bank: "ABBANK",
+    value: 4.8,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 2
+  },
+  {
+    bank: "ABBANK",
+    value: 4.8,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 3
+  },
+  {
+    bank: "ABBANK",
+    value: 6.8,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 6
+  },
+  {
+    bank: "ABBANK",
+    value: 7,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 9
+  },
+  {
+    bank: "ABBANK",
+    value: 7.6,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 12
+  },
+  {
+    bank: "ABBANK",
+    value: 7.7,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 18
+  },
+  {
+    bank: "ABBANK",
+    value: 7.7,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 24
+  },
+  {
+    bank: "ABBANK",
+    value: 7.7,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 36
+  },
+
+  {
+    bank: "Eximbank",
+    value: 7.4,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 12
+  },
+  {
+    bank: "Eximbank",
+    value: 8,
+    threshold: "",
+    lastUpdate: "2020-03-06",
+    period: 36
+  }
 ];
