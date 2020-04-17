3a58df4c8814763505aae8675d0830a439432348
Khang Duy <trinhduykhang1313@gmail.com>
2020-04-17 10:19:50 +0700

    bs

--- controllers/data.js
+++ controllers/data.js
@@ -1,143 +1,172 @@
-var { banks } = require("../models/bank");
-var { rates } = require("../models/rate/import");
-var { rates_seed } = require("../models/rate/seed");
-var { updates } = require("../models/update");
-var { fools } = require("../models/rate/aprilfool");
-var { after } = require("../models/rate/afternoon");
-const { horizons } = require("../models/rate/horizon");
-var bankService = require("../services/bank");
+var { banks } = require('../models/bank')
+var { rates } = require('../models/rate/import')
+var { rates_seed } = require('../models/rate/seed')
+var { updates } = require('../models/update')
+var { fools } = require('../models/rate/aprilfool')
+var { after } = require('../models/rate/afternoon')
+const { horizons } = require('../models/rate/horizon')
+const { bs } = require('../models/rate/bs')
+var bankService = require('../services/bank')
 
 exports.import = (req, res, next) => {
-  let updated = 0;
+  let updated = 0
   banks.find({}, (error, result) => {
     if (error) {
-      res.status(500).send(error);
+      res.status(500).send(error)
     } else if (!result) {
-      res.status(404).send({ error: "cannot find any banks" });
+      res.status(404).send({ error: 'cannot find any banks' })
     } else {
       result.forEach((bank) => {
-        let name = bank.name;
+        let name = bank.name
         if (name) {
-          let rates = rates_seed.filter((e) => e.bank == name);
+          let rates = rates_seed.filter((e) => e.bank == name)
           if (rates && rates.length > 0) {
-            bank.interests = rates;
-            bank.save();
-            updated++;
+            bank.interests = rates
+            bank.save()
+            updated++
           } else {
-            console.log("rates not found for bank " + bank.name);
+            console.log('rates not found for bank ' + bank.name)
           }
         } else {
-          console.log("id not found");
+          console.log('id not found')
         }
-      });
-      res.status(200).json({ updated: `${updated} documents` });
+      })
+      res.status(200).json({ updated: `${updated} documents` })
     }
-  });
-};
+  })
+}
 
 exports.update = async (req, res, next) => {
-  let updated = 0;
+  let updated = 0
   if (req.query.fool) {
-    let banks = await bankService.list();
-    let results = [];
+    let banks = await bankService.list()
+    let results = []
     banks.forEach((bank) => {
-      let interests = fools.filter((e) => e.bank === bank.name);
+      let interests = fools.filter((e) => e.bank === bank.name)
       if (!interests) {
-        console.log("cannot find update for " + bank.name);
+        console.log('cannot find update for ' + bank.name)
       } else {
         if (!bank.interests) {
-          bank.interests = [];
+          bank.interests = []
         }
-        bank.interests = bank.interests.concat(interests);
-        bank.save();
-        results.push(bank);
+        bank.interests = bank.interests.concat(interests)
+        bank.save()
+        results.push(bank)
         updates.findOneAndUpdate(
           {},
           { date: Date.now() },
           { upsert: true, new: true },
           (error, update) => {
             if (error) {
-              console.log(error);
+              console.log(error)
             } else {
             }
           }
-        );
+        )
       }
-    });
-    res.status(200).json({ banks: results });
+    })
+    res.status(200).json({ banks: results })
   } else if (req.query.horizon) {
-    let banks = await bankService.list();
-    let results = [];
+    let banks = await bankService.list()
+    let results = []
     banks.forEach((bank) => {
-      let interests = horizons.filter((e) => e.bank === bank.name);
+      let interests = horizons.filter((e) => e.bank === bank.name)
       if (!interests) {
-        console.log("cannot find update for " + bank.name);
+        console.log('cannot find update for ' + bank.name)
       } else {
         if (!bank.interests) {
-          bank.interests = [];
+          bank.interests = []
         }
-        bank.interests = bank.interests.concat(interests);
-        bank.save();
-        results.push(bank);
+        bank.interests = bank.interests.concat(interests)
+        bank.save()
+        results.push(bank)
         updates.findOneAndUpdate(
           {},
           { date: Date.now() },
           { upsert: true, new: true },
           (error, update) => {
             if (error) {
-              console.log(error);
+              console.log(error)
             } else {
             }
           }
-        );
+        )
       }
-    });
-    res.status(200).json({ banks: results });
+    })
+    res.status(200).json({ banks: results })
+  } else if (req.query.bs) {
+    let banks = await bankService.list()
+    let results = []
+    banks.forEach((bank) => {
+      let interests = bs.filter((e) => e.bank === bank.name)
+      if (!interests) {
+        console.log('cannot find update for ' + bank.name)
+      } else {
+        if (!bank.interests) {
+          bank.interests = []
+        }
+        bank.interests = bank.interests.concat(interests)
+        bank.save()
+        results.push(bank)
+        updates.findOneAndUpdate(
+          {},
+          { date: Date.now() },
+          { upsert: true, new: true },
+          (error, update) => {
+            if (error) {
+              console.log(error)
+            } else {
+            }
+          }
+        )
+      }
+    })
+    res.status(200).json({ banks: results })
   } else if (req.query.after) {
-    let banks = await bankService.list();
-    let results = [];
+    let banks = await bankService.list()
+    let results = []
     banks.forEach((bank) => {
-      let interests = after.filter((e) => e.bank === bank.name);
+      let interests = after.filter((e) => e.bank === bank.name)
       if (!interests) {
-        console.log("cannot find update for " + bank.name);
+        console.log('cannot find update for ' + bank.name)
       } else {
         if (!bank.interests) {
-          bank.interests = [];
+          bank.interests = []
         }
-        bank.interests = bank.interests.concat(interests);
-        bank.save();
-        results.push(bank);
+        bank.interests = bank.interests.concat(interests)
+        bank.save()
+        results.push(bank)
         updates.findOneAndUpdate(
           {},
           { date: Date.now() },
           { upsert: true, new: true },
           (error, update) => {
             if (error) {
-              console.log(error);
+              console.log(error)
             } else {
             }
           }
-        );
+        )
       }
-    });
-    res.status(200).json({ banks: results });
+    })
+    res.status(200).json({ banks: results })
   } else {
     rates.forEach((rate) => {
       if (rate.bank) {
         banks
           .findOne({ normalized: rate.bank.toLowerCase() })
-          .select("interests name")
+          .select('interests name')
           .exec((error, result) => {
             if (error) {
-              failed++;
-              console.error(error);
+              failed++
+              console.error(error)
             } else if (!result) {
-              console.log(`bank ${rate.bank} not found`);
+              console.log(`bank ${rate.bank} not found`)
             } else {
               if (!result.interests) {
-                result.interests = [];
+                result.interests = []
               }
-              result.interests.push(rate);
+              result.interests.push(rate)
               result.save((err, ok) => {
                 if (ok) {
                   updates.findOneAndUpdate(
@@ -146,21 +175,21 @@ exports.update = async (req, res, next) => {
                     { upsert: true, new: true },
                     (error, update) => {
                       if (error) {
-                        console.log(error);
+                        console.log(error)
                       } else {
                       }
                     }
-                  );
-                  updated++;
-                  console.log(`${result.name} updated`);
+                  )
+                  updated++
+                  console.log(`${result.name} updated`)
                 }
-              });
+              })
             }
-          });
+          })
       } else {
-        console.log("invalid rate");
+        console.log('invalid rate')
       }
-    });
-    res.status(200).send({ updated: updated, total: rates.length });
+    })
+    res.status(200).send({ updated: updated, total: rates.length })
   }
-};
+}
